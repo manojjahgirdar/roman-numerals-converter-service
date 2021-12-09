d85011259b8f65ae8d9a81d372422cbd0c33a09e
manojjahgirdar <manoj.jahgirdar@in.ibm.com>
2021-12-09 17:01:16 +0530

    Added contract testing for /to-roman and /to-number APIs

--- test/controllers/to-number.controller.spec.ts
+++ test/controllers/to-number.controller.spec.ts
@@ -1,6 +1,7 @@
 import {Application} from 'express';
 import {default as request} from 'supertest';
 import {Container, Scope} from 'typescript-ioc';
+import { BadRequestError } from 'typescript-rest/dist/server/model/errors';
 
 import {ToNumberApi} from '../../src/services';
 import {buildApiServer} from '../helper';
@@ -16,9 +17,7 @@ describe('to-number.controller', () => {
 
   beforeEach(() => {
     const apiServer = buildApiServer();
-
     app = apiServer.getApp();
-
     Container.bind(ToNumberApi).scope(Scope.Singleton).to(MockToNumberService);
 
     const mockService: ToNumberApi = Container.get(ToNumberApi);
@@ -30,20 +29,42 @@ describe('to-number.controller', () => {
   });
 
   describe('Given /to-number', () => {
-    const expectedResponse = "1";
 
-    beforeEach(() => {
-      mockDeRomanizer.mockReturnValueOnce(Promise.resolve(expectedResponse));
+    describe('When checking for valid roman number input', () => {
+      const romanInput = "IV";
+      const numberOutput = 4;
+
+      beforeEach(() => {
+        mockDeRomanizer.mockImplementation(romanInput => numberOutput);
+      });
+
+      test(`for ${romanInput} it should return ${numberOutput} with content type as text/html and status code 200`, async () => { 
+        await request(app)
+          .get(`/to-number`)
+          .query({value: romanInput})
+          .expect(200)
+          .then((response) => {
+            expect(parseInt(response.text)).toBe(numberOutput);
+            expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
+          });
+      });
     });
 
-    const input = "I";
-    test(`for ${input} it should return 1`, done => {
-      request(app)
-        .get(`/to-number?value=${input}`)
-        .expect(200)
-        .expect(expectedResponse)
-        .end(done);
+    describe('When checking for invalid roman number input', () => {
+      const romanInput = "VIIII";
+
+      beforeEach(() => {
+        mockDeRomanizer.mockImplementation(() => {
+          throw new BadRequestError();
+        });
+      });
+
+      test(`for ${romanInput} it should throw Bad Request Error with status code as 400`, async () => { 
+        await request(app)
+          .get(`/to-number`)
+          .query({value: romanInput})
+          .expect(400);
+      });
     });
   });
-
 });
--- test/controllers/to-roman.controller.spec.ts
+++ test/controllers/to-roman.controller.spec.ts
@@ -1,6 +1,7 @@
 import {Application} from 'express';
 import {default as request} from 'supertest';
 import {Container, Scope} from 'typescript-ioc';
+import { BadRequestError } from 'typescript-rest/dist/server/model/errors';
 
 import {ToRomanApi} from '../../src/services';
 import {buildApiServer} from '../helper';
@@ -30,15 +31,57 @@ describe('to-roman.controller', () => {
   });
 
   describe('Given /to-roman', () => {
-    const expectedResponse = "I";
-    // const expectedResponse = "II";
+    describe('When checking for valid numeric input', () => {
+      const numericInput = 24;
+      const romanOutput = "XXIV";
 
-    beforeEach(() => {
-      mockRomanizer.mockReturnValueOnce(Promise.resolve(expectedResponse));
+      beforeEach(() => {
+        mockRomanizer.mockImplementation(numericInput => romanOutput);
+      });
+
+      test(`for ${numericInput} it should return ${romanOutput} with content type as text/html and status code 200`, async () => { 
+        await request(app)
+          .get(`/to-roman`)
+          .query({value: numericInput})
+          .expect(200)
+          .then((response) => {
+            expect(response.text).toBe(romanOutput);
+            expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
+          });
+      });
+
     });
+    describe('When checking for invalid numeric input', () => {
+      const numericInput = -1;
 
-    test(`for 1 it should return "I"`, done => {
-      request(app).get(`/to-roman?value=${1}`).expect(200).expect(expectedResponse, done);
+      beforeEach(() => {
+        mockRomanizer.mockImplementation(() => {
+          throw new BadRequestError();
+        });
+      });
+
+      test(`for ${numericInput} it should throw Bad Request Error with status code as 400`, async () => { 
+        await request(app)
+          .get(`/to-roman`)
+          .query({value: numericInput})
+          .expect(400);
+      });
+    });
+    describe('When checking for non numeric input', () => {
+      const numericInput = "XIV";
+
+      beforeEach(() => {
+        mockRomanizer.mockImplementation(() => {
+          throw new BadRequestError();
+        });
+      });
+
+      test(`for ${numericInput} it should throw Bad Request Error with status code as 400`, async () => {
+        await request(app)
+          .get(`/to-roman`)
+          .query({value: numericInput})
+          .expect(400);
+      });
     });
   });
 
